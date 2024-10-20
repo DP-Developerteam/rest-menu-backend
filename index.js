69ba16e7eb57a4bca36ad2cd47974c975b7b83df
dp-developer <diegoperez.developer@gmail.com>
2024-10-20 16:17:28 +0200

    export function

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ const path = require('path');
 // Import connection to DB
 const { connect } = require('./api/utils/database/connect.js');
 
-// Express APIs
-const userRoutes = require('./api/routes/user.routes.js');
-const productRoutes = require('./api/routes/product.routes.js');
-
-//Ejecutamos la funcion que conecta con la db
-connect();
-
 // Initialize Express app
 const app = express();
 
@@ -24,6 +17,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+// Express APIs
+const userRoutes = require('./api/routes/user.routes.js');
+const productRoutes = require('./api/routes/product.routes.js');
 
 // Routes
 app.use('/users', userRoutes);
@@ -78,25 +75,23 @@ app.use((err, req, res, next) => {
 //     await connect(); // Ensure DB connection happens first
 // };
 //***   Then we try this
-// let isConnected = false;
+let isConnected = false;
 
-// const init = async () => {
-//     if (!isConnected) {
-//         console.log('Connecting to the database...');
-//         await connect();
-//         isConnected = true;
-//         console.log('Connected to the database');
-//     }
-// };
+const init = async () => {
+    if (!isConnected) {
+        console.log('Connecting to the database...');
+        await connect();
+        isConnected = true;
+        console.log('Connected to the database');
+    }
+};
 
-// module.exports = async (req, res) => {
-//     try {
-//         if (!isConnected) {
-//             await init(); // Only connect if not connected
-//         }
-//         app(req, res); // Pass request and response to Express app
-//     } catch (error) {
-//         console.error('Error during request handling:', error);
-//         res.status(500).json({ message: 'Internal Server Error' });
-//     }
-// };
+module.exports = async (req, res) => {
+    try {
+        await init(); // Only connect if not connected
+        app(req, res); // Pass request and response to Express app
+    } catch (error) {
+        console.error('Error during request handling:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
