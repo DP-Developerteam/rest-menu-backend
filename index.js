4c8d862e3edf061e4b1b33033a92a7616843112c
dp-developer <diegoperez.developer@gmail.com>
2024-10-20 15:59:44 +0200

    exports at bottom

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// Serve public files. styles.css & favicon.ico
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Routes
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
+
+// Serve public files. styles.css & favicon.ico
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve HTML file
 app.get("/", (req, res) => {
@@ -39,6 +39,22 @@ app.use(express.static(__dirname + '/public'))
 
 // Añadimos el favicon
 app.use('/favicon.ico', express.static('public/favicon.ico'));
+
+// 404 Handler for undefined routes
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Route not found"
+    });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    });
+});
+
 
 // // Database Connection and Server Start
 // (async () => {
@@ -71,18 +87,3 @@ module.exports = async (req, res) => {
     await init(); // Connect to DB for each request
     app(req, res); // Pass request and response to Express app
 };
-
-// 404 Handler for undefined routes
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: "Route not found"
-    });
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.statusCode || 500).json({
-        message: err.message || 'Internal Server Error',
-    });
-});
